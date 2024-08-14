92d00bc3a877eacf34ae55b137b1935e33903ef2
Heiko Fanieng <heiko@fanieng.com>
2024-08-14 15:01:36 +0200

    updates

--- payloads/beat.py
+++ payloads/beat.py
@@ -5,12 +5,12 @@ from .base import BasePayload
 class Beat (BasePayload):
     ''' Define the Beat class that inherits from BasePayload '''
 
-    def __init__(self, beat, master, onair, ms, type, version):
+    def __init__(self, beat, master, onair, ms, type_iterable, version):
         super().__init__(ms)
         self.beat = beat
         self.master = master
         self.onair = onair
-        self.type = type
+        self.type_iterable = type_iterable
         self.version = version
 
     @classmethod
@@ -21,9 +21,9 @@ class Beat (BasePayload):
         master = payload.get('master')
         onair = payload.get('onair')
         ms = data.get('ms')
-        type = data.get('type')
+        type_iterable = data.get('type')
         version = data.get('version')
-        return cls(beat, master, onair, ms, type, version)
+        return cls(beat, master, onair, ms, type_iterable, version)
 
     def to_dict(self):
         ''' Convert the Beat object to a dictionary'''
@@ -32,11 +32,11 @@ class Beat (BasePayload):
             'master': self.master,
             'onair': self.onair,
             'ms': self.ms,
-            'type': self.type,
+            'type': self.type_iterable,
             'version': self.version
         }
 
     def __str__(self):
         base_str = super().__str__()
-        return (f"{base_str}, Type:{self.type}, Beat:{self.beat}, Master:{self.master}, Onair:{self.onair}, "
-                f" Version:{self.version})")
+        return (f"{base_str}, Type:{self.type_iterable}, Beat:{self.beat}, Master:{self.master}, "
+                f"  Onair:{self.onair}, Version:{self.version})")
